fa6f3c713872337df22ea7c44080f8c4486e044b
Giang Hoang Van <hvgiang86@gmail.com>
2023-12-08 18:17:05 +0700

    ui/drawer layout & top app bar init

--- application/KSChat/app/src/main/java/vn/id/hvg/kschat/views/screens/MainActivity.kt
+++ application/KSChat/app/src/main/java/vn/id/hvg/kschat/views/screens/MainActivity.kt
@@ -2,22 +2,45 @@ package vn.id.hvg.kschat.views.screens
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.databinding.DataBindingUtil
 import dagger.hilt.android.AndroidEntryPoint
 import vn.id.hvg.kschat.R
+import vn.id.hvg.kschat.databinding.ActivityMainBinding
+import vn.id.hvg.kschat.viewmodels.MainViewModel
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+    private val mainViewModel: MainViewModel by viewModels()
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding =
+            DataBindingUtil.setContentView(this, R.layout.activity_main) as ActivityMainBinding
+        binding.mainViewModel = mainViewModel
+        binding.lifecycleOwner = this
+
+        val actionBar = binding.topAppBar
+        val drawerLayout = binding.myDrawerLayout
+
+        actionBar.setNavigationOnClickListener {
+            binding.myDrawerLayout.open()
         }
+
+        val actionBarDrawerToggle = ActionBarDrawerToggle(
+            this,
+            drawerLayout,
+            R.string.nav_open,
+            R.string.nav_close
+        )
+        drawerLayout.addDrawerListener(actionBarDrawerToggle)
+        actionBarDrawerToggle.syncState()
+
+        actionBar.title = "Chats"
+
+
     }
 }
