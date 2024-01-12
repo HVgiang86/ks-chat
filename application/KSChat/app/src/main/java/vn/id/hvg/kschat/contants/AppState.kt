ef00f3d2615dd38598011f6fc77c589018554f03
Giang Hoang Van <hvgiang86@gmail.com>
2024-01-12 22:47:38 +0700

    move app to another repo

--- application/KSChat/app/src/main/java/vn/id/hvg/kschat/contants/AppState.kt
+++
@@ -1,9 +0,0 @@
-package vn.id.hvg.kschat.contants
-
-enum class LoginState {
-    SUCCESS, INCORRECT_CREDENTIALS, NETWORK_ERROR, UNKNOWN_ERROR
-}
-
-enum class RegisterState {
-    SUCCESS, USERNAME_TAKEN, NETWORK_ERROR, UNKNOWN_ERROR, BAD_REQUEST
-}
--- application/KSChat/app/src/main/java/vn/id/hvg/kschat/data/network/api/ApiEndPoint.kt
+++
@@ -1,5 +0,0 @@
-package vn.id.hvg.kschat.data.network.api
-
-const val LOGIN_ENDPOINT = "auth/login"
-const val REGISTER_ENDPOINT = "auth/register"
-const val REFRESH_TOKEN_ENDPOINT = "auth/refresh-token"
